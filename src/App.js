3d9f83539719a60fa9320c99078b33a44847a6f6
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-04 06:13:29 -0700

    Dropdown menu works!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import axios from 'axios';
+import axios from "axios";
 // useEffect(() => {
 //   console.log("out here!");
 // });
@@ -10,8 +10,10 @@ function App() {
   const [hdurl, setHdurl] = useState();
   const [url, setUrl] = useState();
   const [explanation, setExplanation] = useState();
-  const [today, setToday] = useState(new Date);
-  const [selectDate, setSelectDate] = useState(`${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`);
+  const [today, setToday] = useState(new Date());
+  const [selectDate, setSelectDate] = useState(
+    `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
+  );
 
   // const axios = require('axios');
 
@@ -28,32 +30,126 @@ function App() {
     //     setHdurl(response.data.hdurl);
     //     setUrl(response.data.url);
     //     setExplanation(response.data.explanation);
-        
+
     //   })
     //   .catch(function(error) {
     //     // handle error
     //     console.log(error);
     //   });
   }, [selectDate]);
-
-  function createDates(){
-    let current = new Date;
-    let dateString = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`;
+  let dateArray = [];
+  function createDates() {
+    let current = new Date();
+    let dateString = `${current.getFullYear()}-${current.getMonth() +
+      1}-${current.getDate()}`;
     // console.log(dateString);
     let year = current.getFullYear();
-    let month = current.getMonth()+1;
+    let month = current.getMonth() + 1;
     let day = current.getDate();
-console.log(year,month,day);
 
-    
+    dateArray.push(dateString);
+    for (let i = 0; i < 12; i++) {
+      // console.log(i);
+      for (let i = 0; i < 31; i++) {
+        if (day > 1) {
+          day--;
+          // console.log(day);
+          dateString = `${year}-${month}-${day}`;
+          dateArray.push(dateString);
+        }
+      }
+      if (month > 0) {
+        month--;
+        // dateString = `${year}-${month}-${day}`;
+        // dateArray.push(dateString);
+      }
+      if (day == 1 && month != 1) {
+        switch (month) {
+          case 12:
+            break;
+
+          case 11:
+            day = 31;
+            break;
+
+          case 10:
+            day = 32;
+            break;
+
+          case 9:
+            day = 31;
+            break;
+
+          case 8:
+            day = 32;
+            break;
+
+          case 7:
+            day = 32;
+            break;
+
+          case 6:
+            day = 31;
+            break;
+
+          case 5:
+            day = 32;
+            break;
+
+          case 4:
+            day = 31;
+            break;
+
+          case 3:
+            day = 32;
+            break;
+
+          case 2:
+            if (year % 4 == 0) {
+              if (year % 100 == 0 && year % 400 != 0) {
+                day = 29;
+                break;
+              }
+              day = 30;
+              break;
+            } else {
+              day = 29;
+            }
+            break;
+
+          case 1:
+            day = 32;
+
+            break;
+        }
+      }
+      // console.log(day,month, year);
+
+      if (day == 1 && month == 0) {
+        day = 32;
+        month = 12;
+        year--;
+      }
+      // console.log(day,month, year);
+    }
+    // console.log(dateArray);
+
+    // console.log(year, month, day);
+
     // console.log(today.getFullYear()); //Year YYYY
     // console.log(today.getMonth()+1); //Month MM
     // console.log(today.getDate()); //day of the month DD
+
+    return dateArray;
+
+    // for(let i=0; i<dateArray.length; i++){
+    //   return <option value={dateArray[i]}>{dateArray[i]}</option>
+    // }
   }
 
   createDates();
 
-  function handleSelectDate(){
+  function handleSelectDate() {
     let d = document.getElementById("select_id");
     // console.log(d.value);
     setSelectDate(d.value);
@@ -63,14 +159,22 @@ console.log(year,month,day);
   return (
     <div className="App">
       <select onChange={handleSelectDate} id="select_id">
-        <option value="1">one</option>
+        {/* <option value="1">one</option>
         <option value="2">two</option>
         <option value="3">three</option>
-        <option value="4">four</option>
+        <option value="4">four</option> */}
+        {dateArray.map((item, index) => {
+          return (
+            <option value={item} key={index}>
+              {item}
+            </option>
+          );
+        })}
       </select>
       <h1>{title}</h1>
-      <p>{console.log('rendered')}
-        
+      <p>
+        {console.log("rendered")}
+
         {/* <img src={hdurl}/> */}
         <img className="image" src={url} />
         {explanation}
